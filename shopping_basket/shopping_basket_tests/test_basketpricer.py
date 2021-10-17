50eaa89da0a7b8570400475a4535170d6783d0b4
jakepaton <jakeppaton@gmail.com>
2021-10-17 05:34:14 +0100

    Add tests for discount calculations

--- shopping_basket/shopping_basket_tests/test_basketpricer.py
+++ shopping_basket/shopping_basket_tests/test_basketpricer.py
@@ -1,5 +1,5 @@
 from basket_pricer import BasketPricer
-from offer import Discount, Offer
+from offer import BuyXGetYFree, Discount
 import pytest
 
 
@@ -20,6 +20,14 @@ def basic_catalogue():
 def basic_offers():
     offers = []
     offers.append(Discount(offer_type="Discount", product="Sardines", discount_pc=0.25))
+    offers.append(
+        BuyXGetYFree(
+            offer_type="BuyXGetYFree",
+            product="Baked Beans",
+            quantity_needed_to_buy=2,
+            quantity_get_free=1,
+        )
+    )
     return offers
 
 
@@ -27,17 +35,25 @@ def test_subtotal1(basic_catalogue):
     basket = {"Baked Beans": 4, "Biscuits": 1}
     offers = {}
     pricer = BasketPricer(basket=basket, catalogue=basic_catalogue, offers=offers)
-    assert pricer.subtotal() == 5.16
+    assert pricer.basket_subtotal() == 5.16
 
 
 def test_subtotal2(basic_catalogue):
     basket = {"Baked Beans": 2, "Biscuits": 1, "Sardines": 2}
     offers = {}
     pricer = BasketPricer(basket=basket, catalogue=basic_catalogue, offers=offers)
-    assert pricer.subtotal() == 6.96
+    assert pricer.basket_subtotal() == 6.96
 
 
+# Test of percentage discounting, without any buy x get y free offers
 def test_discount1(basic_catalogue, basic_offers):
-    basket = {"Baked Beans": 4, "Biscuits": 1}
+    basket = {"Biscuits": 1, "Sardines": 2}
     pricer = BasketPricer(basket=basket, catalogue=basic_catalogue, offers=basic_offers)
-    assert pricer.discount() == 0.99
+    assert pricer.basket_discount() == 0.945
+
+
+# Test of buy x get y free offer, without any percentage discounting
+def test_discount2(basic_catalogue, basic_offers):
+    basket = {"Biscuits": 1, "Beans": 5}
+    pricer = BasketPricer(basket=basket, catalogue=basic_catalogue, offers=basic_offers)
+    assert pricer.basket_discount() == 1.98
