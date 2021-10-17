e19dd0d1b35c46e6925b14a18f01536fb67c0ad1
jakepaton <jakeppaton@gmail.com>
2021-10-17 06:54:42 +0100

    Add basket total tests

--- shopping_basket/shopping_basket_tests/test_basketpricer.py
+++ shopping_basket/shopping_basket_tests/test_basketpricer.py
@@ -59,15 +59,25 @@ def test_discount2(basic_catalogue, basic_offers):
     assert abs(pricer.basket_discount() - 0.99) < 0.01
 
 
-# Test of buy x get y free offer, without any percentage discounting
 def test_discount3(basic_catalogue, basic_offers):
     basket = {"Baked Beans": 2, "Biscuits": 1, "Sardines": 2}
     pricer = BasketPricer(basket=basket, catalogue=basic_catalogue, offers=basic_offers)
     assert abs(pricer.basket_discount() - 0.95) < 0.01
 
 
-# Test of buy x get y free offer, without any percentage discounting
-def test_discount3(basic_catalogue, basic_offers):
+def test_discount4(basic_catalogue, basic_offers):
     basket = {"Baked Beans": 10, "Biscuits": 1, "Sardines": 20}
     pricer = BasketPricer(basket=basket, catalogue=basic_catalogue, offers=basic_offers)
     assert abs(pricer.basket_discount() - 12.42) < 0.01
+
+
+def test_total1(basic_catalogue, basic_offers):
+    basket = {"Baked Beans": 4, "Biscuits": 1}
+    pricer = BasketPricer(basket=basket, catalogue=basic_catalogue, offers=basic_offers)
+    assert abs(pricer.basket_total() - 4.17) < 0.01
+
+
+def test_total2(basic_catalogue, basic_offers):
+    basket = {"Baked Beans": 2, "Biscuits": 1, "Sardines": 2}
+    pricer = BasketPricer(basket=basket, catalogue=basic_catalogue, offers=basic_offers)
+    assert abs(pricer.basket_total() - 6.01) < 0.01
